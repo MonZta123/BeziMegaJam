57b7453e80d6add9e9bdfef4f2537c97d444de15
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2026-09-21 13:37:22 -0500

    level and trigger design

--- Assets/Scripts/Gameplay/BossFightStart.cs
+++ Assets/Scripts/Gameplay/BossFightStart.cs
@@ -3,6 +3,7 @@ using UI;
 using UnityEngine;
 using MoreMountains.Feedbacks;
 
+
 public class BossFightStart : MonoBehaviour
 {
     
@@ -23,11 +24,38 @@ public class BossFightStart : MonoBehaviour
     [SerializeField]
     public MMF_Player bouncingFeedback;
 
+    [Header("Floating Variables")]
+    [SerializeField]
+    float amplitude = 0.5f;
+    [SerializeField]
+    float frequency = 1f;
+    [SerializeField]
+    float degreePerSecond = 15.0f;
+    GameObject ingreident;
+    
+    Vector3 startPos;
+    Vector3 endPos;
+
 
     public void Start()
     {
         player = GameObject.FindWithTag("Player").GetComponent<Player>();
-        bouncingFeedback.PlayFeedbacks();
+        ingreident = GetComponentInChildren<IngredientRef>().gameObject;
+        startPos = ingreident.transform.position;
+    }
+
+    public void Update()
+    {
+        if (degreePerSecond != 0)
+        {
+            ingreident.transform.Rotate(new Vector3(0f, degreePerSecond * Time.deltaTime, 0f), Space.World);
+        }
+
+        endPos = startPos;
+        endPos.y += Mathf.Sin(Time.time * Mathf.PI * frequency) * amplitude;
+
+        ingreident.transform.position = endPos;
+        
     }
 
     public void ShowTooltip()
