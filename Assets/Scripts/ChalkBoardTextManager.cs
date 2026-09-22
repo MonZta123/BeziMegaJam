57dfab2d7bf5605be91e00ff65d3bd21b5a14931
jhmauritz <46462219+jhmauritz@users.noreply.github.com>
2026-09-22 11:28:57 -0500

    wokring on level

---
+++ Assets/Scripts/ChalkBoardTextManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class ChalkBoardTextManager : MonoBehaviour
+{
+    [SerializeField]
+    private string[] ComplaintMessages;
+    [SerializeField]
+    private TextMeshPro displayText;
+    private void Start()
+    { 
+        StartCoroutine(ComplaintTimer(5f));
+    }
+
+    private IEnumerator ComplaintTimer(float delay)
+    {
+        while (true)
+        {
+            CycleMessages();
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+    private void CycleMessages()
+    {
+        if (displayText != null || ComplaintMessages.Length > 0)
+        {
+            displayText.text = ComplaintMessages[Random.Range(0, ComplaintMessages.Length)];
+            return;
+        }
+    }
+}
